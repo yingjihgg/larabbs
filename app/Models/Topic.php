72e243215ee260068a9b6c1e488ead47ef656fb9
Yingjihgg <1395381076@qq.com>
2020-04-05 08:20:58 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -52,4 +52,10 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
